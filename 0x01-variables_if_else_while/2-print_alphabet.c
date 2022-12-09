412a3364da015964d34b6677d0244512643dc880
Gee-byte <gichurisarah01@gmai.com>
2022-12-09 10:59:15 -0800

    while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
